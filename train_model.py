e4094e5cc0674661888258452308f73273cdc5b0
Matthew Wrobel <matthew.wrobel@gmail.com>
2023-07-23 15:49:23 -0700

    Adds further logging

--- train_model.py
+++ train_model.py
@@ -1,6 +1,12 @@
 import language_model
+print("Loading neural networks...")
 networks = language_model.load_networks()
+print("Neural networks loaded.")
 language_model.conversion_net = networks["conversion"]
 language_model.generation_net = networks["generation"]
+print("Starting training...")
 language_model.train(language_model.training_set, language_model.generation_net, 1)
+print("Training complete.")
+print("Saving models...")
 language_model.save_models()
+print("Models saved. Program complete.")
